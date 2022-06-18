8ca0e4ce4affdbf312b70f62673cf94d49e76471
Sherzod <daminovsherzod2002@gmail.com>
2022-06-18 12:09:55 +0500

    sum_odd.py

--- sum_odd.py
+++ sum_odd.py
@@ -18,13 +18,13 @@ x3 = var_int%10
 var_int //= 10
 
 x4 = var_int%10
-s1 = x1%2
-s2 = x2%2
-s3 = x3%2
-s4 = x4%2
+s1 = x1 % 2
+s2 = x2 % 2
+s3 = x3 % 2
+s4 = x4 % 2
 b1 = s1 * x1
 b2 = s2 * x2
 b3 = s3 * x3
 b4 = s4 * x4
-sum_even = b1 + b2 + b3 + b4
-print(sum_even)
+y = b1 + b2 + b3 + b4
+print(y)
